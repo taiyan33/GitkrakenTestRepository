e7975da54b8b78e247a0c12a7db5fb8a149103ac
taiyan33 <t7710301988>
2019-09-10 12:54:30 +0800

    add golang example
    first example

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.12
+
+require github.com/appleboy/com v0.0.2 // indirect
---
+++ main.go
@@ -0,0 +1,56 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	_"github.com/weigj/go-odbc"
+
+// 	_ "database/sql"
+// 	// "github.com/weigj/odbc"
+// )
+
+// func main() {
+
+// 	conn, err := sql.Open("GODBC", "driver={SQL Server};SERVER=localhost;UID=sa;PWD=t7710301988;")
+// 	// conn, err := odbc.Connect("DSN=GODBC", "driver={SQL Server};SERVER=localhost;UID=sa;PWD=t7710301988;")
+// 	// conn, err := odbc.Open("DSN=GODBC")
+// 	if err != nil {
+// 		fmt.Println("Connecting Error")
+// 		return
+// 	}
+// 	fmt.Println("Connecting OK")
+// 	defer conn.Close()
+// 	// stmt, err := conn.Prepare("SELECT [ID],[Name],[School],[Sex] FROM [dbo].[Student]");
+// 	// if(err!=nil){
+// 	// fmt.Println("Query Error",err);
+// 	// return;
+// 	// }
+// 	// defer stmt.Close();
+// 	// row,err := stmt.FetchAll();
+// 	// if err!=nil {
+// 	// fmt.Println("Query Error",err);
+// 	// return;
+// 	// }
+// 	// defer row.Close();
+// 	// for row.Next() {
+// 	// var id string;
+// 	// if err := row.Scan(&id);err==nil {
+// 	// fmt.Println(id);
+// 	// }
+// 	// }
+// 	// fmt.Printf("%s\n","finish");
+// 	// return;
+// }
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/appleboy/com/random"
+)
+
+
+func main() {
+	fmt.Println("Hello world A")
+	fmt.Println(random.String(10))
+}
